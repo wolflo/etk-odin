7a1c47997cd860ad3ef85aee4f8f1a8e7f3006f1
wolflo <33909953+wolflo@users.noreply.github.com>
2021-07-23 18:00:02 -0600

    Add preset storage using MockedHost

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
 use etk_asm::ingest::Ingest;
-use evmodin::{*, host::DummyHost, tracing::NoopTracer};
-use ethereum_types::{Address, U256};
+use evmodin::{
+    *,
+    tracing::NoopTracer,
+    util::mocked_host::MockedHost
+};
+use ethereum_types::{Address, U256, H256};
 use hex_literal::hex;
+
 
 #[tokio::main]
 async fn main() {
-    // Bytecode to run.
-    // Note that testing this snippet directly requires control of stack, memory, and returndata.
-    // It expects a memory pointer to an arbitrary prefix on the stack, followed
-    // by the prefix's length. It should return the prefix ++ forwarded returndata.
-    let opcodes = r#"
-        returndatasize
-        push1 0x00
-        dup4
-        dup4
-        add
-        returndatacopy
-        swap1
-        returndatasize
-        add
-        swap1
-        return
-    "#;
+    // The code in code.etk is arbitrary other than the fact that testing
+    // it directly requires control of stack, memory, storage, and returndata.
+    // It expects 3 values on the stack:
+    // 1. A pointer to an arbitrary length "return prefix" in memory
+    // 2. The length of the prefix
+    // 3. A storage slot
+    // It returns the prefix ++ sload(slot) ++ forwarded returndata
 
     // Assemble code with etk-asm
     let mut code = Vec::new();
     let mut ingest = Ingest::new(&mut code);
-    ingest.ingest("", &opcodes).unwrap();
+    ingest.ingest_file("./src/code.etk").unwrap();
 
     // Prepare a message to execute with evmodin
     let message = Message {
         kind: CallKind::Call,
         is_static: true,
         depth: 0,
-        gas: 200,
+        gas: 3000,
         destination: Address::zero(),
         sender: Address::zero(),
         input_data: vec![].into(),
@@ -44,48 +39,51 @@ async fn main() {
 
     // Set initial stack
     // [ bottom, top ]
-    let starting_stack = vec![ 0x04, 0x01 ];
-    let stack = state.stack_mut();
-    for word in starting_stack {
-        stack.push(word.into());
-    }
+    let initial_stack: [U256; 3] = [ 0x42.into(), 0x04.into(), 0x01.into() ];
+    state.stack_mut().0.try_extend_from_slice(&initial_stack).unwrap();
 
     // Set initial memory
-    let starting_mem = vec![ 0x00, 0xba, 0xdf, 0x00, 0xd0 ];
-    *state.memory_mut() = starting_mem.to_vec();
+    let initial_mem = vec![ 0x00, 0xBA, 0xDF, 0x00, 0xD0 ];
+    *state.memory_mut() = initial_mem;
 
     // Set initial returndata (i.e. what's used for RETURNDATA* ops)
-    let starting_return_data = vec![ 0xba, 0xdc, 0xaf, 0xe0 ];
-    *state.return_data_mut() = starting_return_data.into();
+    let initial_return_data = vec![ 0xBA, 0xDC, 0xAF, 0xE0 ];
+    *state.return_data_mut() = initial_return_data.into();
+
+    // Set initial storage (key, value)
+    let initial_storage = [ (0x42, 0xDEADBEEF as u32) ];
+    let host = &mut MockedHost::default();
+    for store in initial_storage {
+        host.set_storage(
+            message.destination,
+            H256(U256::from(store.0).into()),
+            H256(U256::from(store.1).into())
+        ).await.expect("failed to initialize storage");
+    };
 
     // Execute the message with our modified initial state
-    let host = &mut DummyHost;
     let tracer =  &mut NoopTracer;
     let analyzed = AnalyzedCode::analyze(code);
     let result = analyzed.execute_with_state_ext(host, tracer, state).await;
 
+    // Should return the prefix preset in memory ++ the preset storage slot value
+    // ++ the preset returndata
+    let expected_output = hex!("
+        BADF00D0
+        00000000000000000000000000000000000000000000000000000000DEADBEEF
+        BADCAFE0
+    ");
+
     let expected =
         Output {
             status_code: StatusCode::Success,
-            gas_left: 166,
-            output_data: hex!("badf00d0badcafe0").to_vec().into(),
+            gas_left: 839,
+            output_data: expected_output.to_vec().into(),
             create_address: None,
         };
 
     assert_eq!(result.unwrap(), expected);
+
+    // Can also make assertions about the final execution state
+    assert_eq!(state.stack_mut().len(), 2);
 }
-
-// bytecode stack annotation
-//                 //[ ptr prefix_len ]
-// returndatasize  //[ returndatasize ptr prefix_len ]
-// push1 0x00      //[ 0x00 returndatasize ptr prefix_len ]
-// dup4            //[ prefix_len 0x00 returndatasize ptr prefix_len ]
-// dup4            //[ ptr prefix_len 0x00 returndatasize ptr prefix_len ]
-// add             //[ ptr+prefix_len 0x00 returndatasize ptr prefix_len ]
-// returndatacopy  //[ ptr prefix_len ]
-// swap1           //[ prefix_len ptr ]
-// returndatasize  //[ returndatasize prefix_len ptr ]
-// add             //[ returndatasize+prefix_len ptr]
-// swap1           //[ ptr returndatasize+prefix_len]
-// return
-
